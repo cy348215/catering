00ab7c878ae10e7eca06475c29a660a06026eb79
cy348215 <940955751@qq.com>
2019-10-07 15:21:00 +0800

    cy

---
+++ catering_qt/src/main/java/com/catering/controller/MemberController_cy.java
@@ -0,0 +1,7 @@
+package com.catering.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController_cy {
+}
---
+++ catering_qt/src/main/java/com/catering/mapper/MemberMapper_cy.java
@@ -0,0 +1,7 @@
+package com.catering.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper_cy {
+}
