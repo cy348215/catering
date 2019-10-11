6e69d3ba29d430ecdd5a8150a68952a4968c092c
xpy-110 <1102736593@qq.com>
2019-10-11 21:23:59 +0800

    xpy

--- catering_ht/src/main/java/com/catering/controller/ViewController_xpy.java
+++ catering_ht/src/main/java/com/catering/controller/ViewController_xpy.java
@@ -1,15 +1,38 @@
 package com.catering.controller;
 
+import com.catering.pojo.AdminUser;
+import com.catering.service.AdminUserService_xpy;
 import com.catering.service.QueryAllCountService_xpy;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
 
 @Controller
 public class ViewController_xpy {
     @Autowired
     private QueryAllCountService_xpy countService;
+
     @RequestMapping("/login")
     public String login(){
         return "login";
@@ -33,5 +56,40 @@ public class ViewController_xpy {
         model.addAttribute("memuCount",memuCount);
         model.addAttribute("merCount",merCount);
         return "home";
+    }
+    //账号登录验证码
+    @GetMapping("/createImage")
+    public void createImage(HttpServletResponse response, HttpSession session) throws IOException {
+        int width=80,height=30;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        Random r = new Random();
+        g.setColor(new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255)));
+        g.fillRect(0, 0, width, height);
+        //干扰线
+        for (int i = 0; i < 10; i++) {
+            g.setColor(new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255)));
+            g.drawLine(r.nextInt(width), r.nextInt(height), r.nextInt(width), r.nextInt(height));
+        }
+        //获取生成的验证码
+        String code = getNumber();
+        //绑定验证码
+        session.setAttribute("number", code);
+        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 25));
+        g.setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+        g.drawString(code, 5, 25);
+        //设置消息头
+        response.setContentType("image/jpeg");
+        OutputStream os = response.getOutputStream();
+        ImageIO.write(image, "jpeg", os);
+    }
+    private String getNumber(){
+        String str="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String code = "";
+        for(int i= 0;i<4;i++){
+            int index = (int)(Math.random()*str.length());
+            code+=str.charAt(index);
+        }
+        return code;
     }
 }
